7efe86da1e99554e26a6f1b73faa685e4d91a71e
devjunseok <wnslgood58@naver.com>
2022-11-04 17:20:20 +0900

    [추가] 로그아웃, 회원탈퇴 버튼 추가

--- api.js
+++ api.js
@@ -59,10 +59,10 @@ async function handleLogin() {
 
     console.log(response_json)
     if (response.status == 200){
-        localStorage.setItem("access", response_json.access);  // 로컬스토리지안에 access값 저장
-        localStorage.setItem("refresh", response_json.refresh); // 로컬스토리지안에 refresh값 저장
+        localStorage.setItem("access", response_json.access_token);  // 로컬스토리지안에 access값 저장
+        localStorage.setItem("refresh", response_json.refresh_token); // 로컬스토리지안에 refresh값 저장
 
-        const base64Url = response_json.access.split('.')[1];  // 로컬스토리지에 JWT값 저장
+        const base64Url = response_json.access_token.split('.')[1];  // 로컬스토리지에 JWT값 저장
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
         const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
@@ -75,7 +75,32 @@ async function handleLogin() {
 
     }else {
         //로그인 실패시
-        alert(response_json["detail"])
-        window.location.reload();
+        alert(response_json["non_field_errors"])
+        // window.location.reload();
     }
+}
+
+
+async function handleLogout(){
+    localStorage.removeItem("access")
+    localStorage.removeItem("refresh")
+    localStorage.removeItem("payload")
+    alert("로그아웃 성공!")
+        window.location.replace(`${frontEndBaseUrl}/login.html`);
+}
+
+async function handleDelete(){   //mock 함수
+    const response = await fetch('http://127.0.0.1:8000/users/delete/',{
+        headers:{
+            "Authorization":"Bearer " + localStorage.getItem("access")
+        },
+        method:'DELETE',
+    })
+
+    if (response.status ==204){
+        alert("회원탈퇴 완료!")
+        window.location.replace(`${frontEndBaseUrl}/login.html`);
+    }
+
+    console.log(response)
 }
