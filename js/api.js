7656e91f13c5940227836fc3a10bbec69c0be0e9
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2022-11-06 20:52:16 +0900

    [병합]

--- js/api.js
+++ js/api.js
@@ -1,19 +1,15 @@
-const frontEndBaseUrl = "http://127.0.0.1:5502"
-const backEndBaseUrl = "http://127.0.0.1:8000"
 const TmdbApiImageUrl = "https://www.themoviedb.org/t/p/w220_and_h330_face"
-
-// 회원가입
+const frontEndBaseUrl = "http://127.0.0.1:5500"
+const backEndBaseUrl = "http://127.0.0.1:8000"
 window.onload = () => {
     console.log('로딩되었음')
 }
-
 async function handleSignup() {
     const email = document.getElementById("email").value
     const nickname = document.getElementById("nickname").value
     const password1 = document.getElementById("password1").value
     const password2 = document.getElementById("password2").value
     console.log(email, nickname, password1, password2)
-
     const response = await fetch('http://127.0.0.1:8000/users/dj-rest-auth/registration/', {
         headers: {
             'content-type': 'application/json',
@@ -26,27 +22,21 @@ async function handleSignup() {
             "password2": password2
         })
     })
-
     const response_json = await response.json()
-    
     console.log(response)
     if (response.status == 201){
         alert(response_json["detail"])
-            window.location.replace(`${frontEndBaseUrl}/home.html`);
+            window.location.replace(`${frontEndBaseUrl}/login.html`);
     }else {
         alert(response_json["email"])
         alert(response_json["password1"])
         alert(response_json["password2"])
-
     }
 }
-
-// 로그인
 async function handleLogin() {
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
     console.log(email, password)
-
     const response = await fetch('http://127.0.0.1:8000/users/dj-rest-auth/login/', {
         headers: {
             'content-type': 'application/json',
@@ -57,42 +47,32 @@ async function handleLogin() {
             "password": password
         })
     })
-
     const response_json = await response.json()    // responser 값을 json 화
-
     console.log(response_json)
     if (response.status == 200){
         localStorage.setItem("access", response_json.access_token);  // 로컬스토리지안에 access값 저장
         localStorage.setItem("refresh", response_json.refresh_token); // 로컬스토리지안에 refresh값 저장
-
         const base64Url = response_json.access_token.split('.')[1];  // 로컬스토리지에 JWT값 저장
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
         const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
-
         localStorage.setItem("payload", jsonPayload);
-
         alert("로그인 성공!")
             window.location.replace(`${frontEndBaseUrl}/home.html`);
-
     }else {
         //로그인 실패시
         alert(response_json["non_field_errors"])
         // window.location.reload();
     }
 }
-
-// 로그아웃
 async function handleLogout(){
     localStorage.removeItem("access")
     localStorage.removeItem("refresh")
     localStorage.removeItem("payload")
     alert("로그아웃 성공!")
-        window.location.replace(`${frontEndBaseUrl}/index.html`);
+        window.location.replace(`${frontEndBaseUrl}/login.html`);
 }
-
-// 회원탈퇴
 async function handleDelete(){   //mock 함수
     const response = await fetch('http://127.0.0.1:8000/users/delete/',{
         headers:{
@@ -100,46 +80,35 @@ async function handleDelete(){   //mock 함수
         },
         method:'DELETE',
     })
-
     if (response.status ==204){
         alert("회원탈퇴 완료!")
         window.location.replace(`${frontEndBaseUrl}/login.html`);
     }
-
     console.log(response)
 }
-
 async function getMovie(){
     const response = await fetch(`${backEndBaseUrl}/articles/`,{
         method:'GET',
     })
-
     response_json = await response.json()
     return response_json
 }
-
-
-
-async function getMovieDetail(){
-    const response = await fetch(`${backEndBaseUrl}/articles/1/`,{
-
+async function getMovieDetail(movie_id){
+    const response = await fetch(`${backEndBaseUrl}/articles/${movie_id}`,{
         method:'GET',
     })
-
+    //window.location.replace(`${frontEndBaseUrl}/articledetail.html/`);
     response_json = await response.json()
     return response_json
 }
-
-
-
-async function handlePost() {
+async function handlePost(movie_id) {
     const content = document.getElementById("content").value
     const rating = document.getElementById("rating").value
     console.log(content, rating)
-
-    const response = await fetch('http://127.0.0.1:8000/articles/1/comment/', {
+    const response = await fetch(`http://127.0.0.1:8000/articles/${movie_id}/comment/`, {
         headers: {
             'content-type': 'application/json',
+            "Authorization":"Bearer " + localStorage.getItem("access")
         },
         method: 'POST',
         body: JSON.stringify({
@@ -147,7 +116,20 @@ async function handlePost() {
             "rating": rating
         })
     })
+    if (response.status ==200){
+        window.location.reload();
+    }
 }
-
-
-
+async function MovieCommentDelete(comment) {
+    const response = await fetch(`http://127.0.0.1:8000/articles/1/comment/${comment.id}/`, {
+        headers: {
+            'content-type': 'application/json',
+            "Authorization":"Bearer " + localStorage.getItem("access")
+        },
+        method: 'DELETE',
+    })
+    if (response.status ==204){
+        alert("리뷰가 삭제되었습니다!")
+        window.location.reload();
+    }
+}
--- js/articledetail.js
+++ js/articledetail.js
@@ -1,14 +1,16 @@
-
+TmdbApiImageUrl = "https://www.themoviedb.org/t/p/w220_and_h330_face"
 window.onload = async function loadArticle(){
-    articles = await getMovieDetail()
-    console.log(articles)
+    console.log(location);
+    //가공
+    const movie_id = location.search.replace("?", "")
+    //fetch
+    articles = await getMovieDetail(movie_id)
+    //console.log(articles)
     articles_ht = document.getElementById("article")
-
     const ImageMovie = document.createElement("div");
     const movieImage = document.createElement("img")
     const newRating = document.createElement("div");
     const newTitle = document.createElement("div");
-    
     movieImage.setAttribute("src", `${TmdbApiImageUrl}${articles.image}`)
     newTitle.innerText = articles.title
     newRating.innerText = articles.rating
@@ -16,4 +18,20 @@ window.onload = async function loadArticle(){
     articles_ht.appendChild(ImageMovie)
     articles_ht.appendChild(newRating)
     articles_ht.appendChild(newTitle)
+    articles.movie_comment.forEach(comment => {
+        const newContent = document.createElement("div");
+        const newRating = document.createElement("div");
+        const newButton = document.createElement("button");
+        newButton.innerText = "삭제"
+        newButton.onclick=function(){MovieCommentDelete(comment);}
+        newContent.innerText = comment.content
+        newRating.innerText = comment.rating
+        articles_ht.appendChild(newContent)
+        articles_ht.appendChild(newRating)
+        articles_ht.appendChild(newButton)
+        });
+        const btnSubmit = document.getElementById("btn_submit");
+        btnSubmit.onclick=function() {
+            handlePost(movie_id);
+        };
     };
